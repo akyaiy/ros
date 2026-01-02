c63e2146f4b5346b54bd29492e1dc765ef218bd0
Alexey <alex@oblat.lv>
2026-01-03 01:09:07 +0200

    change color

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -71,7 +71,7 @@ lazy_static! {
 	pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
 			column_position: 0,
 			row_position: 0,
-			color_code: ColorCode::new(Color::White, Color::Black),
+			color_code: ColorCode::new(Color::LightGray, Color::Black),
 			buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
 	});
 }
